579eeabd98075e8e0f549b7d922c9b365dee7950
René Meier <meier.rene@googlemail.com>
2026-07-01 12:47:16 +0200

    Add full query mode bach to run script

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ python3 -u create_query_list.py $1 && \
 python3 -u request_by_inchikey.py && \
 python3 -u modify_massbank_data.py $1 false
 
-#python3 -u request.py $2 && \
-#python3 -u modify_massbank_data.py $1 false
+python3 -u create_query_list.py $1 && \
+python3 -u request.py $2 && \
+python3 -u modify_massbank_data.py $1 false
